7080320f92c3a5229ccfd09747de76b4d31ff2f5
PreetiMoo <preetissmoolya15@gmail.com>
2024-08-19 22:33:13 +0530

    vercel.json4

--- routes/index.js
+++ routes/index.js
@@ -8,5 +8,7 @@ router.use("/accounts", require("./accounts"));
 router.use("/transactions/banker", require("./transaction_banker"));
 router.use("/transactions/cust", require("./transaction_customer"));
 router.use("/balance", require("./balance"));
+router.use("/register", require("./register"));
+
 
 module.exports = router;
