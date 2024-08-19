df51596295f37204a21d324076f102e7e1601e01
PreetiMoo <preetissmoolya15@gmail.com>
2024-08-19 14:54:07 +0530

    cors

--- migrations/20240805150745-transaction_table.js
+++ migrations/20240805150745-transaction_table.js
@@ -15,7 +15,7 @@ module.exports = {
         allowNull: false,
       },
       userId: {
-        type: Sequelize.UUID,
+        type: Sequelize.INTEGER,
         allowNull: false,
       },
       amount: {
