cc857819bf14322e8d6112886714ece37dedc5ad
PreetiMoo <preetissmoolya15@gmail.com>
2024-08-19 13:53:57 +0530

    ssl

--- index.js
+++ index.js
@@ -13,6 +13,12 @@ const sequelize = new Sequelize({
   database: process.env.DATABASE_DATABASE,   
   username: process.env.DATABASE_USER, 
   password: process.env.DATABASE_PASSWORD, 
+  dialectOptions: {
+    ssl: {
+      require: true, 
+      rejectUnauthorized: false, 
+    },
+  }
 });
 
 try {
--- sequelize.js
+++ sequelize.js
@@ -3,10 +3,17 @@ const { Sequelize } = require("sequelize");
 
 
 const sequelize = new Sequelize(process.env.DATABASE_DATABASE, process.env.DATABASE_USER, process.env.DATABASE_PASSWORD, {
-  host: process.env.DATABASE_HOST,
-  dialect: "postgres",
-  dialectModule: require('pg')
-});
+    host: process.env.DATABASE_HOST,
+    dialect: "postgres",
+    dialectModule: require('pg'),
+    dialectOptions: {
+      ssl: {
+        require: true, // This will enforce SSL usage
+        rejectUnauthorized: false, // This option can be set to true or false based on your SSL certificate validation needs
+      },
+    },
+  });
+  
 
 
 module.exports = sequelize;
